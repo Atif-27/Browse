cf0207a629b93afced370c927c2151d026411e7a
Atif Ali <atif276ali@gmail.com>
2024-04-13 01:33:19 +0530

    Refactor subscription controller to use mongoose aggregation for fetching subscribers and subscribedTo lists

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -3,7 +3,7 @@ import asyncWrapper from "../utils/asyncWrapper.js";
 import ExpressResponse from "../utils/ExpressResponse.js";
 import ExpressError from "../utils/ExpressError.js";
 import User from "../models/user.model.js";
-
+import mongoose from "mongoose";
 // * Toggle subscription to a user
 const toggleSubscription = asyncWrapper(async (req, res) => {
   const channel = req.params?.channelId;
@@ -39,34 +39,116 @@ const toggleSubscription = asyncWrapper(async (req, res) => {
 // * Get all subscribers of a user
 const getSubscribers = asyncWrapper(async (req, res) => {
   const channel = req.user.id;
-  const subscribers = await Subscription.find({ channel });
-  const subscribersList = subscribers.map(
-    (subscriber) => subscriber.subscriber
-  );
+  // const subscribers = await Subscription.find({ channel });
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channel),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "subscriber",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscriptions",
+              foreignField: "channel",
+              localField: "_id",
+              as: "totalSubscribers",
+            },
+          },
+          {
+            $addFields: {
+              totalSubscribers: { $size: "$totalSubscribers" },
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $project: {
+        _id: 0,
+        subscriber: {
+          _id: 1,
+          username: 1,
+          email: 1,
+          avatar: 1,
+          totalSubscribers: 1,
+        },
+      },
+    },
+  ]);
+
   return res
     .status(200)
     .json(
-      new ExpressResponse(
-        200,
-        { subscribersList, noOfSubscriber: subscribersList.length },
-        "Subscribers fetched successfully"
-      )
+      new ExpressResponse(200, subscribers, "Subscribers fetched successfully")
     );
 });
 
 // * Get all users subscribed to a user
 const getSubscribedTo = asyncWrapper(async (req, res) => {
   const subscriber = req.user.id;
-  const subscribedTo = await Subscription.find({ subscriber });
-  const subscribedToList = subscribedTo.map(
-    (subscribedTo) => subscribedTo.channel
-  );
+
+  const subscribedTo = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: new mongoose.Types.ObjectId(subscriber),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "channel",
+        pipeline: [
+          {
+            $lookup: {
+              from: "videos",
+              localField: "_id",
+              foreignField: "owner",
+              as: "videos",
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$channel",
+    },
+    {
+      $project: {
+        _id: 0,
+        channel: {
+          _id: 1,
+          username: 1,
+          email: 1,
+          avatar: 1,
+          videos: {
+            _id: 1,
+            title: 1,
+            thumbnail: 1,
+            views: 1,
+            duration: 1,
+            createdAt: 1,
+          },
+        },
+      },
+    },
+  ]);
+
   return res
     .status(200)
     .json(
       new ExpressResponse(
         200,
-        { subscribedToList, noOfSubscribedTo: subscribedToList.length },
+        subscribedTo,
         "Subscribed to fetched successfully"
       )
     );
