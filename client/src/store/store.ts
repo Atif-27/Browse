fe689dead77cf78f7805fd90a73e5dfcb982813a
Atif Ali <atif276ali@gmail.com>
2024-04-16 01:09:50 +0530

    Add React Redux hooks and Provider to improve state management in the client

---
+++ client/src/store/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice";
+import channelReducer from "./slices/channelSlice";
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    channel: channelReducer,
+  },
+});
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
