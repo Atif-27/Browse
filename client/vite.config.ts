f2520621883120f9c534918f63b1e410efa13314
Atif Ali <atif276ali@gmail.com>
2024-04-17 16:41:58 +0530

    Add dotenv package to client dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,15 @@
+import dotenv from "dotenv";
 import path from "path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
+dotenv.config();
 export default defineConfig({
   plugins: [react()],
+  // define process env
+  define: {
+    "process.env": process.env,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
